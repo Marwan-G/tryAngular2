080584f0d49fd8f26e6076ace4468e56b51dba96
Marwan-G <ghabin2004@gmail.com>
2017-07-19 20:38:24 +0200

    set up angular component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import {FormsModule} from "@angular/forms";
+import {ServerComponent} from "./server/server.component";
+import { ServersComponent } from './servers/servers.component'
+import { AppAlert } from './alert/alert.component'
+import {myAlertas} from './Alertas/myalertas.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    AppAlert,
+    myAlertas
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
