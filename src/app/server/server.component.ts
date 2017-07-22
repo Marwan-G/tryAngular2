04fdb06281b799b4cb09f1e40b55ef996fbeafc7
Marwan-G <ghabin2004@gmail.com>
2017-07-22 18:58:46 +0200

    styling elenments dynamically with ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -4,10 +4,15 @@ import { Component } from '@angular/core';
   templateUrl:"./server.component.html"
 })
 export class ServerComponent{
-  ServerId: number =10;
-  ServerState: string = " offline  method" ;
-  getServerState(){
-    return this.ServerState;
-  }
-
+    ServerId: number =10;
+    ServerState: string = "offline" ;
+    constructor(){
+      this.ServerState = Math.random() > 0.5 ? "online": "offline";
+    }
+    getServerState(){
+        return this.ServerState;
+    }
+    getColor(){
+      return this.ServerState === 'offline' ? 'red' : 'green';
+    }
 }
